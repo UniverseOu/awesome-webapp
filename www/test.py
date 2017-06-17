ed9ef794ded6da2f612b574ca8fe65b063aeafac
Ocean <263665247@qq.com>
2017-06-17 14:28:24 +0800

    all things but bug in handelers>>Blog.findALL,so I will start agin

--- www/test.py
+++ www/test.py
@@ -1,12 +1,16 @@
-import orm,asyncio
-from model import User, Blog, Comment
+import orm,asyncio,time
+from models import User, Blog, Comment
 
 async def test(loop):
 
-    await orm.create_pool(loop=loop,user='root', password='root', database='awesome')
-    u = User(id='236794',name='ggg', email='ggg@example.com', passwd='0954321', image='about:blank')
-    await u.save()
+	await orm.create_pool(loop=loop,user='root', password='root', db='awesome')
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	
+	b = Blog( id = '5',user_id = '152565',user_name = 'Oceanb7',user_image = 'ssddf',
+	name = 'oceanb',summary = summary,content = 'what the fuck ya',created_at = time.time())
 
+	await b.save()
+	await orm.destory_pool()
 loop = asyncio.get_event_loop()
 loop.run_until_complete(test(loop))
 loop.close()
